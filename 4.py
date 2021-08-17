f555e42c1dba8b871ad01cd900e6fbe111801d0c
TimurWh <88853322+TimurWh@users.noreply.github.com>
2021-08-17 21:45:07 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,5 @@
+number = int(input('Введите число любое кроме 0 и 1'))
+if number % 2 == 0:
+    print('Ништяк')
+else:
+    pass
---
+++ 5.py
@@ -0,0 +1,6 @@
+n = int(input("введити число"))
+if n <= 0:
+    print('ты че')
+    pass
+if n >= 0:
+    print(n+n*n+n*n*n)
