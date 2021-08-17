daee50ad099acecd58f7fa2fc70a37281bd1f1d5
TimurWh <88853322+TimurWh@users.noreply.github.com>
2021-08-17 21:49:49 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,4 +1,5 @@
 name1 = str(input('Как вас зовут?'))
 age = int(input('Сколько вам лет?'))
 age2 = 100 - age
-print('Дорогой', name1,'через', age2,'вам будет 100 лет' )
+new_age = 2021 + age2
+print('Дорогой', name1,'через', new_age,'вам будет 100 лет' )
